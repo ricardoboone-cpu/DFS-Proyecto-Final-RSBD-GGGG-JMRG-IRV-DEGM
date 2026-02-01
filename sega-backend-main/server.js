80fa0100d583dc86321bb775af905625ac1dbe0c
sonicisa456 <isaiasrv2019@outlook.com>
2026-01-31 20:47:30 -0600

    creacion de usuarios al iniciar el server.js

--- sega-backend-main/server.js
+++ sega-backend-main/server.js
@@ -7,10 +7,39 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static("public/images"));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Mongo conectado"))
   .catch(err => console.log(err));
+
+const User = require("./models/User");
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(async () => {
+    console.log("Mongo conectado");
+
+    const users = await User.find();
+    if (users.length === 0) {
+      console.log("Creando usuarios iniciales...");
+
+      await User.create([
+        {
+          username: "eggman",
+          password: "adminPassword",
+          role: "admin"
+        },
+        {
+          username: "metalsonic",
+          password: "userPassword",
+          role: "user"
+        }
+      ]);
+
+      console.log("Usuarios creados");
+    }
+  });
+
 
 // rutas
 app.use('/api/games', require('./routes/games.routes'));
