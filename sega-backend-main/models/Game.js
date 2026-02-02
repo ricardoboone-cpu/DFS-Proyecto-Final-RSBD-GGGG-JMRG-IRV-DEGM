c99a0a493e36d53ce1614aec7483b4b2c5e5e506
sonicisa456 <isaiasrv2019@outlook.com>
2026-02-01 23:55:11 -0600

    cambiamos Game.js para aceptar parametros

--- sega-backend-main/models/Game.js
+++ sega-backend-main/models/Game.js
@@ -1,10 +1,43 @@
 const mongoose = require('mongoose')
 
 const GameSchema = new mongoose.Schema({
-  title: String,
-  genre: String,
-  year: Number,
-  image: String
-})
+  title: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  trailer: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  sinopsis: {
+    type: String
+  },
+  images: [
+    {
+      type: String
+    }
+  ],
+  comprar: [
+    {
+      nombre: String,
+      logo: String,
+      link: String
+    }
+  ]
+});
 
 module.exports = mongoose.model('Game', GameSchema)
